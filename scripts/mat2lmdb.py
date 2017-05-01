7aaf956a26dadb220bca87ac9fca1c7bedcce351
Zhao Kai <zeakey@outlook.com>
2017-05-01 15:16:00 +0800

    Update mat2lmdb.py

--- scripts/mat2lmdb.py
+++ scripts/mat2lmdb.py
@@ -1,7 +1,23 @@
 import numpy as np
 from scipy.io import savemat, loadmat
-import caffe
+# import caffe
 from os.path import join, splitext, split, abspath, isdir
+import shutil, os, argparse
 
-def make_label():
+parser = argparse.ArgumentParser(description='Convert Morph database to LMDB')
+parser.add_argument('--data', type=str, help='Morph database directory', required=True)
+args = args = parser.parse_args()
+def make_db(data, db_path):
+    if isdir(db_path):
+        shutil.rmtree(db_path)
+    os.makedirs(join(db_path, 'image'))
+    os.makedirs(join(db_path, 'label'))
+    # db_img = lmdb.open(join(db_path, 'image'), map_size=int(1e12))
+    # db_lb = lmdb.open(join(db_path, 'image'), map_size=int(1e12))
+    # txn_img, txn_lb = db_img.begin(write=True), db_lb.begin(write=True)
+    items = [i for i in os.listdir(data) if '.mat' in i]
+    print items
+
+if __name__ == '__main__':
+    make_db(args.data, 'data/lmdb')
 	
